af49f7556d3a5e9c7e8016c6b91987682cb011bc
Onur Avan <avanonur@hotmail.com>
2025-01-30 20:00:41 +0300

    app.js added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
@@ -6,9 +11,10 @@ import SignUp from "./pages/SignUp/SignUp";
 const routes = (
   <Router>
     <Routes>
-      <Route path="dashboard" exact element={<Home />} />
-      <Route path="login" exact element={<Login />} />
-      <Route path="signup" exact element={<SignUp />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="dashboard" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
     </Routes>
   </Router>
 );
